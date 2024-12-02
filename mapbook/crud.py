fdb78b0597239eed2c4fdd56df4fea5dea6f5a70
Lacman12 <michal97@poczta.fm>
2024-12-02 12:58:07 +0100

    feat: add create new user

--- mapbook/crud.py
+++ mapbook/crud.py
@@ -7,3 +7,11 @@ def hello(user:str)->None:
 def read_users(users:list)->None:
     for user in users[1:]:  # wybiera wszystkich oprócz 0, ważny : po numerze
         print(f'twój znajomy {user['name']},z miasta {user['city']} opublikował {user['posts']} postów')  # f oznacza format - zastepuje to co jest w nawasie zmienną
+
+
+def add_user(userlist:list)->None:
+    new_name:str=input('Proszę podać imię nowego znajomego ')
+    new_posts:int=int(input('podaj liczbę postów nowego znajomego '))
+    new_city:str=input('Podaj miasto ')
+    new_user: dict = {'name': new_name, 'posts':new_posts, 'city': new_city}
+    userlist.append(new_user)
